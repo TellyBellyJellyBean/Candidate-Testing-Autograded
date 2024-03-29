e1974f9dab8914c2ea6aa357ac1c5ea465332667
Telly Pippa <telly@Tellys-PC.local>
2024-03-29 11:16:51 -0500

    Correct

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,13 +3,23 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = ("")
+// input.question("Hello, candidate! What is your name? ");
+
+// console.log(`\nCandidate Name: ${candidateName}`);
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = ("Who was the first American woman in space? ")
+// input.question("\n1) Who was the first American woman in space? ");
+let correctAnswer = ("Sally Ride");
+let candidateAnswer = ("");
+
+// if (candidateAnswer == correctAnswer){
+//   console.log("")
+// }
 
 
+//console.log(`${candidateName} \n ${question} \n + ${correctAnswer} \n + ${candidateAnswer}`);//
 //TODO: Variables for Part 2
 let questions;
 let correctAnswers;
@@ -18,19 +28,21 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("Hello, candidate! What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+candidateAnswer = input.question(question);
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if (candidateAnswer == correctAnswer) {
+  console.log("Correct!");
+}
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -42,7 +54,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log(`Greetings, ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
